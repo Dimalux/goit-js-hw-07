a232fd8437c25fc04f5f4f983861ebf24be0fde6
Dima_V <astrallux777@ukr.net>
2025-05-16 16:28:26 +0300

    16.05.2025-DZ-07-02

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,13 @@
-'use strict';
+"use strict";
 
-
-const itemAdd = document.querySelectorAll(".item");
+const itemAdd = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${itemAdd.length}`);
 
+itemAdd.forEach((categorie) => {
+  const categorieName = categorie.querySelector("h2").textContent;
 
-
-
-
-
-
+  console.log(`Category: ${categorieName}`);
+  const result = categorie.querySelectorAll("ul li").length;
+  console.log(`Elements: ${result}`);
+  return;
+});
